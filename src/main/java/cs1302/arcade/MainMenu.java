542fd91b6e3b692b99af79a797b7bdf48e3cc3ec
Raleigh Dial <raleigh.dial@gmail.com>
2019-11-24 19:56:16 -0500

    Added animation test on main menu

--- src/main/java/cs1302/arcade/MainMenu.java
+++ src/main/java/cs1302/arcade/MainMenu.java
@@ -5,6 +5,11 @@ import javafx.stage.Stage;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.animation.Timeline;
+import javafx.animation.KeyFrame;
+import javafx.event.*;
+import javafx.util.Duration;
+import javafx.scene.text.TextAlignment;
 
 public class MainMenu {
 
@@ -21,17 +26,84 @@ public class MainMenu {
     String chessText = "Press '1' to play Chess";
     String astText = "Press '2' to play Asteroids";
     String quitText = "Press 'Q' to exit";
-
+    String[] art = {
+        "                         <>\n" +
+        "                        <==>\n" +
+        "                       <====>\n",
+        "                      <======>\n" +
+        "                     <==={}===>\n" +
+        "                    <==={..}===>\n",
+        "                   <==={....}===>\n" +
+        "                  <==={......}===>\n" +
+        "                 <==={...()...}===>\n",
+        "                <==={...{()}...}===>\n" +
+        "               <==={...{{()}}...}===>\n" +
+        "              <==={...{{{()}}}...}===>\n",
+        "             <==={...{{{(@@)}}}...}===>\n" +
+        "            <==={...{{{(@@@@)}}}...}===>\n" +
+        "           <==={...{{{(@@**@@)}}}...}===>\n",
+        "          <==={...{{{(@@****@@)}}}...}===>\n" +
+        "         <==={...{{{(@@**##**@@)}}}...}===>\n" +
+        "        <==={...{{{(@@**####**@@)}}}...}===>\n",
+        "       <==={...{{{(@@**##<>##**@@)}}}...}===>\n" +
+        "      <==={...{{{(@@**##<==>##**@@)}}}...}===>\n" +
+        "     <==={...{{{(@@**##<====>##**@@)}}}...}===>\n",
+        "    <==={...{{{(@@**##<======>##**@@)}}}...}===>\n" +
+        "   <==={...{{{(@@**##<==={}===>##**@@)}}}...}===>\n" +
+        "  <==={...{{{(@@**##<==={..}===>##**@@)}}}...}===>\n",
+        " <==={...{{{(@@**##<==={....}===>##**@@)}}}...}===>\n" +
+        "<==={...{{{(@@**##<==={......}===>##**@@)}}}...}===>\n" +
+        "==={...{{{(@@**##<==={...()...}===>##**@@)}}}...}===\n",
+        "=={...{{{(@@**##<==={...{()}...}===>##**@@)}}}...}==\n" +
+        "={...{{{(@@**##<==={...{{()}}...}===>##**@@)}}}...}=\n" +
+        "{...{{{(@@**##<==={...{{{()}}}...}===>##**@@)}}}...}\n",
+        "...{{{(@@**##<==={...{{{(@@)}}}...}===>##**@@)}}}...\n" +
+        "..{{{(@@**##<==={...{{{(@@@@)}}}...}===>##**@@)}}}..\n" +
+        ".{{{(@@**##<==={...{{{(@@**@@)}}}...}===>##**@@)}}}.\n",
+        "{{{(@@**##<==={...{{{(@@****@@)}}}...}===>##**@@)}}}\n" +
+        "{{(@@**##<==={...{{{(@@**##**@@)}}}...}===>##**@@)}}\n" +
+        "{(@@**##<==={...{{{(@@**####**@@)}}}...}===>##**@@)}\n",
+        "(@@**##<==={...{{{(@@**##<>##**@@)}}}...}===>##**@@)\n" +
+        "@@**##<==={...{{{(@@**##<==>##**@@)}}}...}===>##**@@\n" +
+        "@**##<==={...{{{(@@**##<====>##**@@)}}}...}===>##**@\n",
+        "**##<==={...{{{(@@**##<======>##**@@)}}}...}===>##**\n" +
+        "*##<==={...{{{(@@**##<==={}===>##**@@)}}}...}===>##*\n" +
+        "##<==={...{{{(@@**##<==={..}===>##**@@)}}}...}===>##\n",
+        "#<==={...{{{(@@**##<==={....}===>##**@@)}}}...}===>#\n" +
+        "<==={...{{{(@@**##<==={......}===>##**@@)}}}...}===>\n" +
+        "==={...{{{(@@**##<==={...()...}===>##**@@)}}}...}===\n",
+        "=={...{{{(@@**##<==={...{()}...}===>##**@@)}}}...}==\n" +
+        "={...{{{(@@**##<==={...{{()}}...}===>##**@@)}}}...}=\n" +
+        "{...{{{(@@**##<==={...{{{()}}}...}===>##**@@)}}}...}\n"
+    };
+    Text artText = new Text(art[0]);
+    int count = 1;
+    Timeline timeline;
     public MainMenu() {
         welcome = new Text(welcText);
         chess = new Text(chessText);
         asteroids = new Text(astText);
         quit = new Text(quitText);
-        vbox = new VBox(welcome, chess, asteroids, quit);
+        EventHandler<ActionEvent> handler = event -> {
+            updateText();
+        };
+        KeyFrame keyFrame = new KeyFrame(Duration.seconds(0.5), handler);
+        timeline = new Timeline();
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.getKeyFrames().add(keyFrame);
+        timeline.play();
+        artText.setTextAlignment(TextAlignment.CENTER);
+        vbox = new VBox(welcome, chess, asteroids, quit, artText);
         root = new HBox(vbox);
         menuScene = new Scene(root, 640, 480);
     }
-
+    public void updateText() {
+        if(count >= art.length) {
+            count = 0;
+        } //if
+        artText.setText(art[count]);
+        count++;
+    } //updateText
     public HBox getRoot() {
         return root;
     }
