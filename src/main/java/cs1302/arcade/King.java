f4f77e9aafde425139d2be3b21d8f0d8b1fb39f4
Raleigh Dial <raleigh.dial@gmail.com>
2019-11-26 16:13:39 -0500

    Added individual piece classes

---
+++ src/main/java/cs1302/arcade/King.java
@@ -0,0 +1,14 @@
+package cs1302.arcade;
+
+public class King extends ChessPiece {
+
+    public King(boolean isWhite, int row, int col) {
+        super(isWhite, row, col);
+    } //King
+
+    public boolean canMove() {
+        //TODO: Movement variables
+        return false;
+    } //canMove
+
+} //King
---
+++ src/main/java/cs1302/arcade/Pawn.java
@@ -0,0 +1,14 @@
+package cs1302.arcade;
+
+public class Pawn extends ChessPiece {
+
+    public Pawn(boolean isWhite, int row, int col) {
+        super(isWhite, row, col);
+    } //Pawn
+
+    public boolean canMove() {
+        //TODO: Movement variables
+        return false;
+    } //canMove
+
+} //Pawn
