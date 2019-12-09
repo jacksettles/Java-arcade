2fc9d04fc46e2c4b6baf01b1297c052cb6688891
Raleigh Dial <raleigh.dial@gmail.com>
2019-12-08 22:15:35 -0500

    Bishop stuff

--- src/main/java/cs1302/arcade/Bishop.java
+++ src/main/java/cs1302/arcade/Bishop.java
@@ -3,28 +3,108 @@ package cs1302.arcade;
 import javafx.scene.layout.GridPane;
 import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.Color;
 
 public class Bishop extends ChessPiece {
 
     GridPane chessGrid;
+    ChessPiece[][] board;
+    Rectangle[] possibleMoves = new Rectangle[13];
 
     public Bishop(boolean isWhite, int row, int col, GridPane chessGrid, ChessPiece[][] board) {
         super(isWhite, row, col, chessGrid, board);
         this.chessGrid = chessGrid;
+        this.board = board;
         this.getRect().setOnMouseClicked(move());
+        for (int i = 0; i < 13; i++) { //13 max possible moves for a bishop
+            possibleMoves[i] = new Rectangle(40, 40, Color.GRAY);
+            possibleMoves[i].setOnMouseClicked(replace(i));
+        } //for
     } //Bishop
 
     private EventHandler<? super MouseEvent> move() {
         return event -> {
-            if (this.canMove()) {
-            chessGrid.getChildren().remove(r);
-            } //if
-        };
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    if (this.board[i][j] != null) {
+                        this.board[i][j].getRect().setDisable(true);
+                    } //if
+                } //for
+            } //for
+            canMove();
+        }; //return
     } //move
 
-    public boolean canMove() {
-        //TODO: Movement variables
-        return true;
+    private EventHandler<? super MouseEvent> replace(int index) {
+        return event -> {
+            for(int i = 0; i < 13; i++) {
+                if(i != index) {
+                    chessGrid.getChildren().remove(possibleMoves[i]);
+                } //if
+            } //for
+            chessGrid.getChildren().remove(this.getRect());
+            this.board[row][col] = null;
+            this.row = GridPane.getRowIndex(possibleMoves[index]);
+            this.col = GridPane.getColumnIndex(possibleMoves[index]);
+            chessGrid.getChildren().remove(possibleMoves[index]);
+            if (this.board[this.row][this.col] != null) {
+                chessGrid.getChildren().remove(board[row][col].getRect());
+            } //if
+            this.setRow(row);
+            this.setCol(col);
+            chessGrid.add(this.getRect(), col, row);
+            this.board[row][col] = this;
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    if (this.board[i][j] != null) {
+                        this.board[i][j].getRect().setDisable(false);
+                        this.board[i][j].setBoard(this.board);
+                    } //if
+                } //for
+            } //for
+        }; //return
+    } //move
+
+    public void canMove() {
+        boolean moved = false;
+        boolean stopLoop = false;
+        this.board = this.getBoard();
+        int index = 0;
+        for (int row = this.getRow(); row < 8; row++) { //FIX ALL THIS NEED RATIO
+            for (int col = this.getCol(); col < 8; col++) {
+                if (row == col) {
+                    if (!stopLoop) {
+                        if (this.board[row][col] == null) {
+                            chessGrid.add(possibleMoves[index], col, row);
+                            moved = true;
+                            index++;
+                        } else if (this.board[row][col].isWhite() != this.isWhite()) {
+                            chessGrid.add(possibleMoves[index], col, row);
+                            moved = true;
+                            stopLoop = true;
+                            index++;
+                        } //if
+                    } //if
+                } //if
+            } //for
+        } //for
+        stopLoop = false;
+
+
+        if (!moved) {
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    if (this.board[i][j] != null) {
+                        this.board[i][j].getRect().setDisable(false);
+                        this.board[i][j].setBoard(this.board);
+                    } //if
+                } //for
+            } //for
+        } else {
+            this.setClicked(true);
+        } //if
     } //canMove
+
 
 } //Bishop
